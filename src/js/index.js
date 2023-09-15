9b0c4d4f9f9fcf5f58fb16ca326a17b0ba849fe6
ViktorHromadskyi <130694874+ViktorHromadskyi@users.noreply.github.com>
2023-09-15 20:37:05 +0300

    Summary

--- src/js/index.js
+++ src/js/index.js
@@ -1,59 +1,115 @@
+import './sass/index.scss';
+import NewsApiService from './js/api-service';
+import { lightbox } from './js/lightbox';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import { onScroll } from './scroll.js';
-import { createMarkup } from './markup.js';
-import { servisePixabay } from './servisePixabay.js';
 
-const form = document.querySelector('.search-form');
-const gallery = document.querySelector('.gallery');
-const btnLoadMore = document.querySelector('.load-more-hidden');
-let page = 1;
-let lightbox = new SimpleLightbox('.gallery a');
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  galleryContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+let isShown = 0;
+const newsApiService = new NewsApiService();
 
-form.addEventListener('submit', fetchPhotos);
-btnLoadMore.addEventListener('click', onLoadMore);
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-async function fetchPhotos(e) {
-  try {
-    e.preventDefault();
-    gallery.innerHTML = '';
-    document.body.classList.remove('gradient');
-    btnLoadMore.classList.replace('load-more', 'load-more-hidden');
+const options = {
+  rootMargin: '50px',
+  root: null,
+  threshold: 0.3,
+};
+const observer = new IntersectionObserver(onLoadMore, options);
 
-    page = 1;
-    const arrPhotos = await servisePixabay(page);
-    createMarkup(arrPhotos.hits);
+function onSearch(element) {
+  element.preventDefault();
 
-    document.body.classList.add('gradient');
-    Notify.success(`Hooray! We found ${arrPhotos.totalHits} images.`);
-    lightbox.refresh();
-  } catch (err) {
-    console.log('TRY-CATCH:', err);
+  refs.galleryContainer.innerHTML = '';
+  newsApiService.query =
+    element.currentTarget.elements.searchQuery.value.trim();
+  newsApiService.resetPage();
+
+  if (newsApiService.query === '') {
+    Notify.warning('Please, fill the main field');
+    return;
+  }
+
+  isShown = 0;
+  fetchGallery();
+  onRenderGallery(hits);
+}
+
+function onLoadMore() {
+  newsApiService.incrementPage();
+  fetchGallery();
+}
+
+async function fetchGallery() {
+  refs.loadMoreBtn.classList.add('is-hidden');
+
+  const result = await newsApiService.fetchGallery();
+  const { hits, total } = result;
+  isShown += hits.length;
+
+  if (!hits.length) {
     Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
+      `Sorry, there are no images matching your search query. Please try again.`
     );
-    gallery.innerHTML = '';
-    document.body.classList.remove('gradient');
-    btnLoadMore.classList.replace('load-more', 'load-more-hidden');
+    refs.loadMoreBtn.classList.add('is-hidden');
+    return;
+  }
+
+  onRenderGallery(hits);
+  isShown += hits.length;
+
+  if (isShown < total) {
+    Notify.success(`Hooray! We found ${total} images !!!`);
+    refs.loadMoreBtn.classList.remove('is-hidden');
+  }
+
+  if (isShown >= total) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
   }
 }
 
-async function onLoadMore() {
-  page += 1;
-
-  const arrPhotos = await servisePixabay(page);
-  createMarkup(arrPhotos.hits);
-  onScroll();
+function onRenderGallery(elements) {
+  const markup = elements
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+    <a href="${largeImageURL}">
+      <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+    </a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        ${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        ${downloads}
+      </p>
+    </div>
+    </div>`;
+      }
+    )
+    .join('');
+  refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
   lightbox.refresh();
-  // console.log(page * arrPhotos.hits.length);
-  if (
-    page * arrPhotos.hits.length >= arrPhotos.totalHits ||
-    page * arrPhotos.hits.length === 0
-  ) {
-    btnLoadMore.classList.replace('load-more', 'load-more-hidden');
-    Notify.info("We`re sorry, but you've reached the end of search results.");
-  }
 }
-
-export { gallery, btnLoadMore, form };
